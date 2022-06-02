455bef8de10305b9953d1be6275528db5a45059a
Aliice77 <69522330+Aliice77@users.noreply.github.com>
2022-06-02 21:54:10 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace oop
+
+{
+
+public class Customer {
+private int _costom;
+public int costom
+{
+    get { return _costom; }
+    set { _costom = value; }
+}
+public int aa(){
+                int b=costom*7;
+            Console.WriteLine(b);
+            return b;
+}
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer objCustomer = new Customer();
+            objCustomer.costom=Convert.ToInt32( Console.ReadLine());
+            objCustomer.aa();
+        }
+    }
+}
